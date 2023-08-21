34326c2dfabfe06aa7fd39f3de20193b98801f0d
Sarvanpilli <sarvansrisaipilli@gmail.com>
2023-08-21 22:01:57 +0530

    arr

---
+++ array.java
@@ -0,0 +1,8 @@
+public class array {
+    public static void main(String[] args) {
+        //int num[] = {3,7,2,8};
+        int num[] = new int[4];
+        System.out.println(num[0]+num[1]);
+    }
+    
+}
